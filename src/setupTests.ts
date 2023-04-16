2973db796d5e6357ee789df778a8c2da0613cc64
Stephen Haberman <stephen.haberman@gmail.com>
2023-04-16 12:48:17 -0500

    tests: Add query tracking.

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -5,11 +5,18 @@ import "src/setupIt";
 
 let appContext: AppContext | undefined = undefined;
 let appContextPromise: Promise<AppContext>;
+export let numberOfQueries = 0;
+export let queries: string[] = [];
 
 export async function getAppContext(): Promise<AppContext> {
   if (!appContext) {
-    appContextPromise ??= newAppContext().then((ctx) => (appContext = ctx));
-    await appContextPromise;
+    appContextPromise ??= newAppContext().then((ctx) => {
+      ctx.knex.on("query", (e: any) => {
+        queries.push(e.sql);
+      });
+      return ctx;
+    });
+    appContext = await appContextPromise;
   }
   return appContext!;
 }
@@ -26,11 +33,22 @@ afterAll(async () => {
 type ContextOpts = Partial<Context>;
 
 export async function createTestContext(opts: ContextOpts = {}): Promise<Context> {
+  resetQueries();
   const appContext = await getAppContext();
   const ctx = { ...appContext };
   const em = opts.em ?? new EntityManager(ctx as Context, appContext.driver);
   const req = null!;
   return Object.assign(ctx, { em, req });
+}
+
+export async function newEntityManager() {
+  resetQueries();
+  const appContext = await getAppContext();
+  return new EntityManager(appContext as Context, appContext.driver);
+}
+
+export function resetQueries() {
+  queries = [];
 }
 
 expect.extend({ toMatchEntity });
