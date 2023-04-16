25d1731c5eddc609fbab09aba2d7ff2debd2092c
Stephen Haberman <stephen.haberman@gmail.com>
2023-04-16 13:37:22 -0500

    fix: Use yarn dlx to avoid asdf errors.

--- watch-joist.sh
+++ watch-joist.sh
@@ -5,7 +5,7 @@ set -e
 JOIST_HOME="${JOIST_HOME:-../joist-ts}"
 
 for package in orm codegen graphql-codegen graphql-resolver-utils test-utils ; do
-  npx copy-and-watch --watch "$JOIST_HOME/packages/$package/build/**/*" ./node_modules/joist-$package/build/ &
+  yarn dlx copy-and-watch --watch "$JOIST_HOME/packages/$package/build/**/*" ./node_modules/joist-$package/build/ &
 done
 
 sleep infinity
