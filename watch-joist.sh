2e954fe10c90f596a017bc0bd2b22e8deeae61a6
Stephen Haberman <stephen.haberman@gmail.com>
2023-10-28 12:30:26 -0500

    Add utils/migration-utils to watch.

--- watch-joist.sh
+++ watch-joist.sh
@@ -4,9 +4,9 @@ set -e
 
 JOIST_HOME="${JOIST_HOME:-../joist-ts}"
 
-for package in orm codegen graphql-codegen graphql-resolver-utils test-utils ; do
+for package in orm codegen graphql-codegen graphql-resolver-utils utils test-utils migration-utils; do
   yarn dlx copy-and-watch --watch "$JOIST_HOME/packages/$package/build/**/*" ./node_modules/joist-$package/build/ &
 done
 
-sleep infinity
+sleep 2147483647
 
